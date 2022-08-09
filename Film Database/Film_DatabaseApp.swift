7b887be5a4e963532195d7afedf74e3474965616
Sean Nimmo <seandnimmo@gmail.com>
2022-08-09 11:24:28 -0400

    Initial Commit

---
+++ Film Database/Film_DatabaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Film_DatabaseApp.swift
+//  Film Database
+//
+//  Created by Sean Nimmo on 8/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Film_DatabaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
